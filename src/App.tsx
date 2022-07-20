0b413a971ed660b42c1eb50f23e1f8ab088861bc
vinitecnico <vinitecnico@gmail.com>
2022-07-19 22:26:20 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import "./loading.css";
 
 function App() {
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsSuccess(true), 5000);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className={`loader ${isSuccess ? 'success' : ''}`} />
+      {/* <span className={`loader ${isSuccess ? 'fail' : ''}`} /> */}
     </div>
   );
 }
